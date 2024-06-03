d3c70b7434fd72cead182ed444c6e4a743b0601e
itait8 <itait8@gmail.com>
2024-06-03 19:28:34 +0300

    added all components, added chat service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'ng-chat';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ChatContainerComponent } from './components/chat-container/chat-container.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatContainerComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'chat/roomId',
+    component: ChatContainerComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
